304a06e9fc81ef601f27dc734c365967d9ec3faa
Pipe Jara <pipejarauribeboteroa@gmail.com>
2024-11-03 14:30:07 -0500

    inicio con title y css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import { Title } from "./components/Title";
-import { CardItem } from "./components/CardItem";
-import { DragAndDrop } from "./components/DragAndDrop";
 
 const App = () => {
   return (
     <div className="container-main flex">
       <Title />
-      <DragAndDrop />
     </div>
   );
 };
