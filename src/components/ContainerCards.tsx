1242dbc5be495b5b697edd4b7dee26354d79a912
Pipe Jara <pipejarauribeboteroa@gmail.com>
2024-11-07 21:33:57 -0500

    sin Drag

---
+++ src/components/ContainerCards.tsx
@@ -0,0 +1,26 @@
+import { Data, Status } from "../interfaces";
+import { CardItem } from "./CardItem";
+
+interface Props {
+  items: Data[];
+  status: Status;
+}
+
+export const ContainerCards = ({ items = [], status }: Props) => {
+  return (
+    <div className="layout-cards">
+      <p>{status} hero</p>
+      {
+        items.map(
+          item => (
+            status === item.status && 
+            <CardItem 
+              data={item} 
+              key={item.id} 
+            />
+          )
+        )
+      }
+    </div>
+  );
+};
