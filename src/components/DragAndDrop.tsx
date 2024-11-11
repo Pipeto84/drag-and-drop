59a138a7002c4d3d886f529af88c637b6e9d0b39
Pipe Jara <pipejarauribeboteroa@gmail.com>
2024-11-10 23:15:22 -0500

    custom hook

--- src/components/DragAndDrop.tsx
+++ src/components/DragAndDrop.tsx
@@ -1,44 +1,26 @@
-import { Data, Status } from "../interfaces";
+import { Status } from "../interfaces";
 import { ContainerCards } from "./ContainerCards";
-import { data } from '../assets'
-import { useState } from "react";
+import { data } from "../assets";
+import { useDragAndDrop } from '../hooks/useDragAndDrop'
 
 const typeHero: Status[] = ["good", "bad", "normal"];
 
 export const DragAndDrop = () => {
 
-  const [isDragging, setIsDragging] = useState(false)
-  const [listItems, setListItems] = useState<Data[]>(data)
-
-  const handleDragging = (dragging: boolean) => setIsDragging(dragging)
-
-  const handleUpdateList = (id: number, status: Status) => {
-    let card = listItems.find(item => item.id === id)
-
-    if(card && card.status !== status) {
-      card.status = status
-      setListItems(prev => ([
-        card!,
-        ...prev.filter(item => item.id !== id)
-      ]))
-    }
-  }
+  const {isDragging, listItems, handleDragging, handleUpdateList} = useDragAndDrop(data)
 
   return (
     <div className="grid">
-      {
-        typeHero.map((container) => (
-          <ContainerCards 
-            items={listItems} 
-            status={container} 
-            key={container} 
-
-            isDragging={isDragging}
-            handleDragging={handleDragging}
-            handleUpdateList={handleUpdateList}
-          />
-        ))
-      }
+      {typeHero.map((container) => (
+        <ContainerCards
+          items={listItems}
+          status={container}
+          key={container}
+          isDragging={isDragging}
+          handleDragging={handleDragging}
+          handleUpdateList={handleUpdateList}
+        />
+      ))}
     </div>
   );
 };
