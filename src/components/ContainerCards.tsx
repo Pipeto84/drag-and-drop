00b4197dd8137ccc1fbabdeaf577c160ab1f5527
Pipe Jara <pipejarauribeboteroa@gmail.com>
2024-11-10 19:38:22 -0500

    drop

--- src/components/ContainerCards.tsx
+++ src/components/ContainerCards.tsx
@@ -1,5 +1,6 @@
-import { Status, Data } from '../interfaces'
-import { CardItem } from './CardItem'
+import React from "react";
+import { Status, Data } from "../interfaces";
+import { CardItem } from "./CardItem";
 
 interface Props {
   status: Status;
@@ -9,21 +10,41 @@ interface Props {
   handleUpdateList: (id: number, status: Status) => void;
 }
 
-export const ContainerCards = ({status, items = [], isDragging, handleDragging}: Props) => {
-  return (
-    <div className={`layout-cards ${isDragging ? 'layout-dragging' : ''}`}>
-      <p>{status} hero</p>
-      {
+export const ContainerCards = ({
+  status,
+  items = [],
+  isDragging,
+  handleDragging,
+  handleUpdateList
+}: Props) => {
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+  };
 
-        items.map(item => (
-          status === item.status &&
-          <CardItem
-            data={item}
-            key={item.id}
-            handleDragging={handleDragging}
-          />
-        ))
-      }
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    let id = +e.dataTransfer.getData('text')
+    handleUpdateList(id, status);
+    handleDragging(false)
+  };
+
+  return (
+    <div
+      className={`layout-cards ${isDragging ? "layout-dragging" : ""}`}
+      onDragOver={handleDragOver}
+      onDrop={handleDrop}
+    >
+      <p>{status} hero</p>
+      {items.map(
+        (item) =>
+          status === item.status && (
+            <CardItem
+              data={item}
+              key={item.id}
+              handleDragging={handleDragging}
+            />
+          )
+      )}
     </div>
-  )
-}
+  );
+};
